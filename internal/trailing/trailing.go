a4d85c88f270634a4178bcc8dca901b2bdef781f
snakoner <andrstroganov@yandex.ru>
2022-12-12 15:25:51 +0300

    Update trailing.go

--- internal/trailing/trailing.go
+++ internal/trailing/trailing.go
@@ -3,7 +3,7 @@ package trailing
 import "github.com/go-binance-robot/internal/robot"
 
 func RecalcTrailingStop(ts *robot.Trade, diff float64) bool {
-	if diff > 0 {
+	if diff > 0.0 {
 		ts.StopLossValue += diff
 		ts.TakeProfitValue += diff
 		return true
