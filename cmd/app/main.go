e025cfe044b5c8ecf0811ef21cb73e5870a7971e
snakoner <andrstroganov@yandex.ru>
2022-12-12 09:21:13 +0300

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"sync"
-
 	"github.com/go-binance-robot/internal/binance"
 	"github.com/go-binance-robot/internal/robot"
 )
 
-var _ = fmt.Sprintf("%d", 0)
-
-
 func main() {
-	r := robot.New()
-	robot.Run(r)
+	robot.New().Run()
 }
