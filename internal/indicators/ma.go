e08e75c09eb1cc80570d61c310fe2088c9f6857a
snakoner <andrstroganov@yandex.ru>
2022-12-12 09:50:55 +0300

    add Sma()

--- internal/indicators/ma.go
+++ internal/indicators/ma.go
@@ -1,14 +1,27 @@
 package indicators
 
+// Simple moving average
+func Sma(data []float64, n int) []float64 {
+	result := make([]float64, len(data) - n + 1)
+	for i := 0; i < len(result); i++ {
+		for _, val := range data[i:i+n] {
+			result[i] += val
+		}
+		result[i] /= n
+	}
+	
+	return result
+}
+
 // Exponential moving average
 func Ema(data []float64, n int) []float64 {
-	emaRet := make([]float64, len(data))
-	emaRet[0] = data[0]
+	result := make([]float64, len(data))
+	result[0] = data[0]
 	alpha := 2./(1. + float64(n))
 
 	for i := 1; i < len(data); i++ {
-		emaRet[i] = alpha * data[i] + (1. - alpha) *emaRet[i - 1]
+		result[i] = alpha * data[i] + (1. - alpha) *result[i - 1]
 	}
 
-	return emaRet
+	return result
 }
