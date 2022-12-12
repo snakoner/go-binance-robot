e5e95ce2ddee38a29e6707fbe161acd6fb14b8b8
snakoner <andrstroganov@yandex.ru>
2022-12-12 10:35:38 +0300

    Update divergence.go

--- internal/indicators/divergence.go
+++ internal/indicators/divergence.go
@@ -1,5 +1,36 @@
 package indicators
 
+type stockPoint struct {
+	index int
+	price float64
+}
+
+func findLocalMins(data []float64) []stockPoint {
+	stockPoints := []stockPoint{}
+	negativeAreas := [][]float64{}
+	negativeAreasTemp := []float64{}
+
+	for i, val := range data {
+		if val < 0 {
+			negativeAreasTemp = append(negativeAreasTemp, val)
+		} else if x >= 0 {
+			negativeAreas = append(negativeAreas, negativeAreasTemp)
+			negativeAreasTemp = nil
+		}
+	}
+
+	if len(negativeAreasTemp) != 0 {
+		negativeAreas = append(negativeAreas, negativeAreasTemp)
+	}
+
+	for _, area := range negativeAreas {
+
+	}
+
+	return stockPoints
+}
+
+
 func Divergence(data []float64) (bool, bool) {
 	return false, false
 }
