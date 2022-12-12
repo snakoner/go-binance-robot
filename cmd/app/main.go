20325fb5dc0c86924569de05e478e54709113099
snakoner <andrstroganov@yandex.ru>
2022-12-12 09:17:20 +0300

    remove Run() declaration from main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,21 +11,8 @@ import (
 
 var _ = fmt.Sprintf("%d", 0)
 
-func Run() {
-	wg := &sync.WaitGroup{}
-	r := robot.New()
-	wg.Add(len(r.Tokens))
-	for i := range r.Tokens {
-		go func(wg *sync.WaitGroup, ts *robot.Trade) {
-			binance.WebSocketRun(ts, 500)
-			wg.Done()
-			log.Printf("%s socket finished\n", ts.Token)
-		}(wg, &r.TradingSession[i])
-	}
-	wg.Wait()
-}
 
 func main() {
-	// symbols := []string{"ADA", "AVAX", "ETH", "MATIC", "SOL", "FTM"}
-	Run()
+	r := robot.New()
+	robot.Run(r)
 }
